9e8e4d9f2e5eb2bf292858467322c53c329fffbf
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-11-11 13:31:35 -0300

    assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { SobreComponent } from './view/sobre/sobre.component';
+import { ProdutosComponent } from './view/produtos/produtos.component';
+import { HomeComponent } from './view/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'cadastro', component: CadastroComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'sobre', component:SobreComponent},
+  {path: 'produtos', component:ProdutosComponent},
 ];
 
 @NgModule({
