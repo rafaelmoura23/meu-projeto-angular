3b1b7a2b29b9d2b3e0b616bfb01f43d0b9bbb874
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-11-11 13:06:04 -0300

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './view/home/home.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { ProdutosComponent } from './view/produtos/produtos.component';
+import { SobreComponent } from './view/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
     LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProdutosComponent,
+    SobreComponent,
   ],
   imports: [
     BrowserModule,
