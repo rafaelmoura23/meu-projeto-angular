d24172db2c2cccf55d0471872c4a04740bd6289b
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-12-02 22:02:41 -0300

    save

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   apiUrl = 'http://localhost:3000/api/login'; // Defina a rota de login no seu servidor
+  isLoggedIn: boolean = false;
+  username: string = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
 
   login(username: string, password: string) {
-    return this.http.post(this.apiUrl, { username, password });
+    // Realiza a chamada de login para a API
+    return this.http.post(this.apiUrl, { username, password }).pipe(
+      tap(() => {
+        this.isLoggedIn = true; // Define o estado de login como verdadeiro após uma resposta bem-sucedida
+        this.username = username;
+      })
+    );
   }
+
+  logout() {
+    // Realiza o logout, definindo o estado de login como falso
+    this.isLoggedIn = false;
+    this.username = '';
+    this.router.navigate(['/login']);
+  }
+
+  getUsername(): string {
+    return this.username;
+  }
+
 }
+
--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  showDropdown: boolean = false;
+  nomeUsuario: string = '';
 
-}
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  toggleDropdown(): void {
+    this.showDropdown = !this.showDropdown;
+    this.nomeUsuario = this.authService.getUsername();
+  }
+
+    logout(): void {
+      this.authService.logout();
+    }
+  }
